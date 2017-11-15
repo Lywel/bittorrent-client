10d74ffc03b58127fba1da509f4d7cb71f244aba
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2017-11-15 10:43:18 +0100

    reauest_track cleaning code

--- src/request_tracker.c
+++ src/request_tracker.c
@@ -7,28 +7,28 @@
 /**
  * 117 beeing the length of peer_id + info_hash + port added to all the
  * nescessary keywords related to the GET post
-*/ 
+*/
 #define C_SIZE 118
 
 static char *
 request_create(struct be_node *dico)
 {
   char *peer_id = peer_id_generate();
-  char *announce = dico_get(dico, "announce");
-  char *info_hash = dico_get(dico, "info_hash");
-  char *port = dico_get(dico, "port");
+  char *announce = dico_get_str(dico, "announce");
+  char *info_hash = dico_get_str(dico, "info_hash");
+  char *port = dico_get_str(dico, "port");
   char *bytes_left = bytes_left_get();
   char *bytes_dwl = bytes_dwl_get();
   char *bytes_upl = bytes_upl_get();
+  size_t req_len = C_SIZE + strlen(bytes_left) + strlen(bytes_dwl)
+                          + strlen(bytes_upl) + strlen(announce);
 
-  char *request = calloc(C_SIZE + strlen(bytes_left) + strlen(bytes_dwl)
-                                + strlen(bytes_upl) + strlen(announce), 1);
+  char *request = calloc(req_len, sizeof(char));
 
   sprintf(request, "%s?peer_id=%s&info_hash=%s&port=%s&left=%s&downloaded=%s&"
                    "uploaded=%s&compact=1",
                    announce, peer_id, info_hash, port, bytes_left, bytes_dwl,
                    bytes_upl);
-
   return request;
 }
 
