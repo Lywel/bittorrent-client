3d676048f2bf0f54e36d76605e82b552590f0323
Martin Noel <martin.noel@epita.fr>
2017-11-16 13:20:03 +0100

    init_socket : initializing socket on right port

--- src/client.c
+++ src/client.c
@@ -4,13 +4,22 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <netdb.h>
 #include <netinet/in.h> //check if legit
 #include <arpa/inet.h>
 
+static uint16_t port;
+
+uint16_t
+get_port(int socket)
+{
+  return ntohs(port);
+}
+
 /**
- * init a new socket and returns the ports it's init on
+ * init a new socket and returns its file descriptor
  */
-uint32_t
+int
 init_socket(void)
 {
   struct sockaddr_in *client = malloc(sizeof(struct sockaddr_in));
@@ -25,12 +34,19 @@ init_socket(void)
   client->sin_family = AF_INET;
   client->sin_addr.s_addr = INADDR_ANY;
 
-  uint32_t port = 6881;
-  client->sin_port = htons(port);
+  uint16_t portt = 6881;
+  client->sin_port = htons(portt);
 
   for (; bind(sock, (struct sockaddr *)client, sizeof(client)) > 0
-                    && port <= 6889; ++port)
-    client->sin_port = htons(port);
+                    && port <= 6889; ++portt)
+    client->sin_port = htons(portt);
 
-  return port;
+  port = client->sin_port;
+  return sock;
 }
+/*
+int main(void)
+{
+  int sock = init_socket();
+  uint16_t portt = get_port(sock);
+}*/
