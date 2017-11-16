fe0073de6f274cfaa7e4d5f52bc7745a8e20aa35
Martin Noel <martin.noel@epita.fr>
2017-11-16 16:49:29 +0100

    peer_id : modified peer_id starting name

--- src/peer_id.c
+++ src/peer_id.c
@@ -6,8 +6,8 @@ char *
 generate_peer_id(void)
 {
   char *peer_id = calloc(21, sizeof(char));
-  strcpy(peer_id, "MB2020");
-  for (size_t i = 6; i < 20; ++i)
+  strcpy(peer_id, "-MB2020-");
+  for (size_t i = 8; i < 20; ++i)
     peer_id[i] = 'A' + (rand() % 26);
 
   return peer_id;
