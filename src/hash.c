c4e97a57ff22d24d4ef0111bcb7a3e12872649d5
Martin Noel <martin.noel@epita.fr>
2017-11-16 20:56:55 +0100

    compute_sha1 : now using EVP_DIGEST

--- src/hash.c
+++ src/hash.c
@@ -1,10 +1,17 @@
-#include <openssl/sha.h>
+#include <openssl/evp.h>
 #include <stdlib.h>
 
-unsigned char *
-compute_sha1(unsigned char *data)
+void
+compute_sha1(const unsigned char *message, size_t message_len,
+             unsigned char **digest)
 {
-  unsigned char *hash = calloc(SHA_DIGEST_LENGTH + 1, sizeof(char));
-  SHA1(data, sizeof(data), hash);
-  return hash;
+  EVP_MD_CTX *mdctx;
+  unsigned int len = 20;
+
+  mdctx = EVP_MD_CTX_create();
+  EVP_DigestInit_ex(mdctx, EVP_sha1(), NULL);
+  *digest = (unsigned char *)OPENSSL_malloc(EVP_MD_size(EVP_sha1()));
+  EVP_DigestFinal_ex(mdctx, *digest, &len);
+
+  EVP_MD_CTX_destroy(mdctx);
 }
