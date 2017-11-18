c13c0fc69eda0138af49bbbf38795a4ad3f3a527
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2017-11-18 23:24:46 +0100

    verbose : adding verbose function

--- src/debug.c
+++ src/debug.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "debug.h"
+#include "client.h"
 
 void debug(char *fmt, ...)
 {
@@ -10,11 +11,20 @@ void debug(char *fmt, ...)
 #if DEBUG
   va_list args;
   va_start(args, fmt);
-  char *fmtfmt = "==DEBUG== %s\n";
-  char *format = calloc(8 + strlen(fmtfmt) + strlen(fmt) + 1, sizeof(char));
+  char *fmtfmt = KCYN "==DEBUG== %s\n" KNRM;
+  char *format = calloc(strlen(fmtfmt) + strlen(fmt) + 1, sizeof(char));
   sprintf(format, fmtfmt, fmt);
   vfprintf(stderr, format, args);
   free(format);
   va_end(args);
 #endif
+}
+
+void verbose(char *fmt, ...)
+{
+  va_list args;
+  va_start(args, fmt);
+  if (g_bt.verbose)
+    vprintf(fmt, args);
+  va_end(args);
 }
