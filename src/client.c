e88f09dbc2a3c255aa9dc9096a8af699fbe8f970
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2017-11-19 16:02:25 +0100

    refactoring : compute_sha1 this function doesnt need its own header

--- src/client.c
+++ src/client.c
@@ -1,10 +1,35 @@
+#include <openssl/sha.h>
 #include <stdlib.h>
 #include "buffer.h"
 #include "peer_id.h"
 #include "bencode.h"
-#include "hash.h"
 #include "client.h"
 #include "dico_finder.h"
+
+static char *
+compute_sha1(s_buf *info)
+{
+  unsigned char *hash = calloc(20, sizeof(char));
+  SHA1((unsigned char *)info->str, info->len, hash);
+  return (char *)hash;
+}
+
+/*
+void
+compute_sha1(s_buf *info, unsigned char **digest)
+{
+  EVP_MD_CTX *mdctx;
+  unsigned int len = 20;
+
+  mdctx = EVP_MD_CTX_create();
+  EVP_DigestInit_ex(mdctx, EVP_sha1(), NULL);
+  *digest = (unsigned char *)OPENSSL_malloc(EVP_MD_size(EVP_sha1()));
+  EVP_DigestUpdate(mdctx, info->str, info->len);
+  EVP_DigestFinal_ex(mdctx, *digest, &len);
+
+  EVP_MD_CTX_destroy(mdctx);
+}
+*/
 
 void
 init_client(void)
--- src/include/hash.h
+++
@@ -1,8 +0,0 @@
-#ifndef HASH_H
-# define HASH_H
-
-#include "buffer.h"
-
-char *compute_sha1(s_buf *info);
-
-#endif
