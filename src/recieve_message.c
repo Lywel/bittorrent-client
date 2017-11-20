f130ba8b94e166b656c8019c006cbbe2f2601f6a
Maxime Lewandowski <lewandowski.mmjb@gmail.com>
2017-11-20 23:19:18 +0100

    recieve_data : now raw data message are handled as is and not as unsuported messages

--- src/recieve_message.c
+++ src/recieve_message.c
@@ -43,7 +43,27 @@ handle_bitfield(struct message mess, struct peer *p)
   return -1 * !!(g_bt.pieces_len != mess.len - 1);
 }
 
-int
+static int
+recieve_data(struct message mess, struct peer *p)
+{
+  if (mess.id != 7)
+    fwrite(&mess, 1, sizeof(struct message), stdout);
+
+  char buf[4094];
+  ssize_t read = 0;
+  ssize_t offset = 0;
+  while ((read = recv(p->sfd, buf, 1024, 0)) > 0)
+  {
+    debug("bytes %d to %d:", offset, offset + read);
+    fwrite(buf, 1, read, stdout);
+    puts("");
+    offset += read;
+    //write_file(buf, len, p->req_blk, p->req_offset);
+  }
+  return read;
+}
+
+static int
 recieve_piece(struct message mess, struct peer *p)
 {
   // verify if we finished a block
@@ -61,18 +81,7 @@ recieve_piece(struct message mess, struct peer *p)
   debug("recieving block %u of size %u beggining at %u",
     ntohl(piece.index), blk_len, ntohl(piece.begin));
 
-  char buf[4094];
-  ssize_t read = 0;
-  ssize_t offset = 0;
-  while ((read = recv(p->sfd, buf, 1024, 0)) > 0)
-  {
-    debug("bytes %d to %d:", offset, offset + read);
-    fwrite(buf, 1, read, stdout);
-    puts("");
-    offset += read;
-    //write_file(buf, len, p->req_blk, p->req_offset);
-  }
-  return 0;
+  return recieve_data(mess, p);
 }
 
 static int
@@ -104,6 +113,8 @@ handle_message(struct message mess, struct peer *p)
     return handle_bitfield(mess, p);
   case 7:
     return recieve_piece(mess, p);
+  default:
+    return recieve_data(mess, p);
   }
   return 0;
 }
